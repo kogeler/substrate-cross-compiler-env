29757304fd5712f4b8cd9d8a393b6c5872e7807d
kogeler <kogeler@gmail.com>
2025-03-28 08:47:55 +0200

    add more binaries

--- build-env.sh
+++ build-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd dockerfiles
-docker build --pull --no-cache --file Dockerfile-env-${2} --build-arg "DEBIAN_VERSION=${1}" -t "rust-env-debian-${1}-${2}" .
+docker build --load --pull --no-cache --file Dockerfile-env-${2} --build-arg "DEBIAN_VERSION=${1}" -t "rust-env-debian-${1}-${2}" .
